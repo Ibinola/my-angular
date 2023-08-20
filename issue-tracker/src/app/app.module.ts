35d5d11d8d43cf9b5907a980972e2cd27fea617b
Michael Ibinola <ibinolamichael1@gmail.com>
2023-08-20 23:41:47 +0100

    feat: issue-report form

--- issue-tracker/src/app/app.module.ts
+++ issue-tracker/src/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {ClarityModule} from '@clr/angular';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { IssueListComponent } from './issue-list/issue-list.component'
+import {ReactiveFormsModule} from '@angular/forms';
+import { IssueReportComponent } from './issue-report/issue-report.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueListComponent,
+    IssueReportComponent
   ],
   imports: [
     BrowserModule,
     ClarityModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
